f43841c373f05886dc902dbea16994073fd181a5
Romane G <romane71193@gmail.com>
2019-08-15 10:24:14 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import React, { Component } from 'react';
+import { ReactiveBase } from '@appbaseio/reactivesearch';
+class Main extends React.Component {
+  render() {
+    return (
+      <div className="main-container">
+        <ReactiveBase  app="mrcarscraig"
+          credentials="3paTsKrNe:0f731b8d-e4d0-44b6-b77a-8fd95c018aa1"
+         >
+          Hello from ReactiveSearch!
+        </ReactiveBase>
+      </div>
+    );
+  }
+}
+export default Main;
+
+
+
